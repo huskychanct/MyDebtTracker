06e8917101e8e5763d3ef4971516070e780963c6
Jacob <jacob@Jacobs-MacBook-Pro.local>
2020-07-12 14:02:45 -0400

    First commit..

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>
+                My Debt Tracker
+            </h2>
+        </div>
+    )
+}
---
+++ src/Components/PaymentsExpenses.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+ 
+ export const PaymentsExpenses = () => {
+     return (
+         <div class="pay-exp-container">
+             <div>
+                 <h4> Payments</h4>
+                 <p id="money-plus" class="money plus">+$0.00</p>
+             </div>
+             <div>
+                 <h4>Debt</h4>
+                 <p id="money-minus" class="money minus">-$0.00</p>
+             </div>
+         </div>
+     )
+ }
+ 
